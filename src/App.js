c1a847a9b06fee96e59f161e5451fe5b2e5cefaa
Pankaj8839 <pankajrajput8839@gmail.com.>
2024-11-12 01:39:09 +0530

    change file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [result, setResult] = useState('');
+  const [expression, setExpression] = useState('');
+  
+  
+  const handleClick = (value) => {
+    if (value === '=') {
+      if(expression === ''){
+        setResult('Error');
+        return;
+      };
+      setResult(eval(expression));
+      return;
+    } else if (value === 'C') {
+      setExpression('');
+      setResult('');
+      return;
+    }
+    setExpression(expression + value);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>React Calculator</h1>
+    <input type="text" value={expression} onChange={(e) => setExpression(e.target.value)} />
+    <p className="result-text">{result}</p>
+    <div className="button-container">
+      <button className="button" onClick={()=>{handleClick('7')}} >7</button>
+      <button className="button" onClick={()=>{handleClick('8')}} >8</button>
+      <button className="button" onClick={()=>{handleClick('9')}} >9</button>
+      <button className="button" onClick={()=>{handleClick('+')}} >+</button>
+      <button className="button" onClick={()=>{handleClick('4')}} >4</button>
+      <button className="button" onClick={()=>{handleClick('5')}} >5</button>
+      <button className="button" onClick={()=>{handleClick('6')}} >6</button>
+      <button className="button" onClick={()=>{handleClick('-')}} >-</button>
+      <button className="button" onClick={()=>{handleClick('1')}} >1</button>
+      <button className="button" onClick={()=>{handleClick('2')}} >2</button>
+      <button className="button" onClick={()=>{handleClick('3')}} >3</button>
+      <button className="button" onClick={()=>{handleClick('*')}} >*</button>
+      <button className="button" onClick={()=>{handleClick('C')}} >C</button>
+      <button className="button" onClick={()=>{handleClick('0')}} >0</button>
+      <button className="button" onClick={()=>{handleClick('=')}} >=</button>
+      <button className="button" onClick={()=>{handleClick("/")}} >/</button>
+    </div>
     </div>
   );
 }
